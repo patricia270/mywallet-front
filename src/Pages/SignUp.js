3ec2bfd0fb33c480acbcdfba4564b70548cca67b
patriciasilva.eq@gmail.com <patriciasilva.eq@gmail.com>
2021-10-23 18:32:33 -0400

    Sign Up route layout

---
+++ src/Pages/SignUp.js
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom';
+import {
+    ContainerForm,
+    Form,
+    Title,
+    Input,
+    ModelButton,
+    SimpleButton
+} from '../Styles/StyledComponents';
+
+function SignUp() {
+    return (
+        <ContainerForm>
+            <Form>
+                <Title>MyWallet</Title>
+                <Input type="text" placeholder="Nome" />
+                <Input type="email" placeholder="E-mail" />
+                <Input type="password" placeholder="Senha" />
+                <Input type="password" placeholder="Confirme a senha" />
+                <ModelButton>Cadastrar</ModelButton>
+            </Form>
+            <Link to='/'>
+                <SimpleButton>Já tem uma conta? Entre agora!</SimpleButton>
+            </Link>
+        </ContainerForm>
+    );
+}
+
+export default SignUp;
---
+++ src/Styles/StyledComponents.js
@@ -0,0 +1,70 @@
+import styled from "styled-components";
+
+const ContainerForm = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+const Form = styled.form`
+    display: flex;
+    width: 86.9vw;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 95px;
+    margin-bottom: 32px;
+    text-align: center;
+`;
+
+const Title = styled.h1`
+    font-size: 32px;
+    color: #FFFFFF;
+    font-family: 'Saira Stencil One', cursive;
+    margin-bottom: 20px;
+`;
+
+const Input = styled.input`
+    margin-top: 13px;
+    width: 86.9vw;
+    height: 58px;
+    color: #000000;
+    font-size: 20px;
+    border-radius: 5px;
+    padding-left: 15px;
+`;
+
+const ModelButton = styled.button`
+    margin-top: 13px;
+    /* width: 326px; */
+    width: 86.9vw;
+    height: 46px;
+    background-color: #A328D6;
+    border-radius: 5px;
+    font-size: 20px;
+    color: #FFFFFF;
+    font-family: 'Raleway', sans-serif;
+    font-weight: bold;
+    &:hover {
+        cursor: pointer;
+        filter: brightness(110%);
+    }
+`;
+const SimpleButton = styled.button`
+    font-size: 15px;
+    width: 86.9vw;
+    font-family: 'Raleway', sans-serif;
+    color: #FFFFFF;
+    font-weight: bold;
+    background-color: transparent;
+    &:hover {
+        cursor: pointer;
+    }
+`;
+
+export {
+    ContainerForm,
+    Form,
+    Title,
+    Input,
+    ModelButton,
+    SimpleButton
+};
